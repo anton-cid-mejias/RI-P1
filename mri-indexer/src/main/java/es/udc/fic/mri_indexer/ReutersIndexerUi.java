396930d166b900aa56696c6da7e8d4e37063c97a
pedro-rivero <pedrorivero19@gmail.com>
2017-03-05 18:17:40 +0100

    a ver si se sube ahora

--- mri-indexer/src/main/java/es/udc/fic/mri_indexer/ReutersIndexerUi.java
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/ReutersIndexerUi.java
@@ -14,12 +14,14 @@ public class ReutersIndexerUi {
 	    String usage =
 		      "Usage: Indexing options [-openmode openmode] [-index pathname] [-coll pathname] "
 		      + "[-colls pathname_1 ... pathname_n] [-indexes1 pathname_0 pathname_1 ... pathname_n] "
-		      + "[-indexes2 pathname_0] \n"
+		      + "[-indexes2 pathname_0] \n";
+	    /*
 		      + "Index proccessing options [-indexin indexfile] [-best_idfterms field n] "
 		      + "[-poor_idfterms field n] [-best_tfidfterms field n] [-poor_tfidfterms field n] \n"
 		      + "Creation of new Index from old one [-indexin indexfile] [-indexout indexfile] "
 		      + "[-deldocsterm field term] [-mostsimilardoc_title h] [-mostsimilardoc_body n h] "
 		      + "[-mostsimilardoc_title h y -mostsimilardoc_body n h d] \n";
+	     */
 
 	    if ((args.length == 0) || (args.length > 0 && ("-h".equals(args[0]) || "-help".equals(args[0])))){
 		print_usage_and_exit();
