b11e6a9fb40ea803ee308a814022568bd6d497b7
anton-cid-mejias <anton.cid.mejias@gmail.com>
2017-03-05 17:56:39 +0100

    Continued with the work in Indexer

--- mri-indexer/src/main/java/es/udc/fic/mri_indexer/Indexer.java
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/Indexer.java
@@ -1,25 +1,37 @@
 package es.udc.fic.mri_indexer;
 
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
 public class Indexer {
 
-    private String openmode;
+    private OpenMode openmode;
     private String index;
     private String coll;
-    private String colls;
-    private String indexes1;
-    private String indexes2;
 
-    public Indexer(String openmode, String index, String coll, String colls,
-	    String indexes1, String indexes2) {
+    public Indexer(OpenMode openmode, String index, String coll) {
 	this.openmode = openmode;
 	this.index = index;
 	this.coll = coll;
-	this.colls = colls;
-	this.indexes1 = indexes1;
-	this.indexes2 = indexes2;
     }
 
-    public void run(){
+    public void run() throws IOException{
+	System.out.println("Indexing to directory '" + index + "'...");
 	
+	Directory dir = FSDirectory.open(Paths.get(index));
+	Analyzer analyzer = new StandardAnalyzer();
+	IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
+	
+	iwc.setOpenMode(openmode);
+	
+	IndexWriter writer = new IndexWriter(dir, iwc);
     }
 }
