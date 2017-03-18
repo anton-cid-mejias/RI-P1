0c192298479014b432134e8f59df819e14964aad
pedro-rivero <pedrorivero19@gmail.com>
2017-03-18 19:59:25 +0100

    mess

--- mri-indexer/src/main/java/es/udc/fic/mri_indexer/MostSimilarDoc_TitleThreading.java
+++ mri-indexer/src/main/java/es/udc/fic/mri_indexer/MostSimilarDoc_TitleThreading.java
@@ -3,6 +3,7 @@ package es.udc.fic.mri_indexer;
 import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Set;
+import java.util.StringTokenizer;
 import java.util.TreeSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -18,31 +19,23 @@ import org.apache.lucene.index.IndexOptions;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.IndexWriterConfig.OpenMode;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.queries.mlt.MoreLikeThis;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanQuery;
+import org.apache.lucene.search.BooleanClause.Occur;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
-
-/*
- * 
- * OJO; TAL Y COMO ESTÁ CREO QUE NO RULA BIEN; LA DISTRIBUCION DE DOCUMENTOS POR THREAD
- * O ALGUNOS HACEN TRABAJO DE MÁS O ALGO ASÍ
- * 
- * 
- * 
- */
-
-
-
 
 public class MostSimilarDoc_TitleThreading {
 
     private static class WorkerThread implements Runnable {
 
-	// esta copiada y falta modificar
 	private final int initialDoc;
 	private final int finalDoc;
 	private final DirectoryReader reader;
@@ -61,47 +54,75 @@ public class MostSimilarDoc_TitleThreading {
 	    Document doc = null;
 	    Document queryDoc = null;
 	    String queryString = null;
-	    Query query = null;
+	    BooleanQuery query = null;
+	    BooleanQuery.Builder bqBuilder = null;
+	    Query query1 = null;
+	    Query query2 = null;
 	    QueryParser parser = new QueryParser("TITLE",
-			new StandardAnalyzer());
+		    new StandardAnalyzer());
 	    IndexSearcher searcher = new IndexSearcher(reader);
+	    //
+	    MoreLikeThis mlt = new MoreLikeThis(reader);
+	    mlt.setAnalyzer(new StandardAnalyzer());
+	    String[] fieldNames = new String[2];
+	    fieldNames[0] = "TITLE";
+	    fieldNames[1] = "BODY";
+	    mlt.setFieldNames(fieldNames);
+	    //
 	    TopDocs topDocs = null;
-	    /*
-	    FieldType t1 = new FieldType();
-	    t1.setIndexOptions(IndexOptions.DOCS_AND_FREQS);
-	    t1.setTokenized(true);
-	    t1.setStored(true);
-	    t1.setStoreTermVectors(true);
-	    t1.freeze();
-	    */
 	    Field SimPathSgmField = null;
 	    Field SimTitle = null;
 	    Field SimBody = null;
-	    
+
 	    try {
-		
-		for (int i = initialDoc; i < finalDoc; i++) {
+		for (int i = initialDoc; i <= finalDoc; i++) {
 		    doc = reader.document(i);
-		    queryString = "TITLE:" + "\"" + doc.get("TITLE") +"\""
-			    + " AND BODY:" + "\"" + doc.get("TITLE") +"\"";
-		    query = parser.parse(queryString);
-		    
+		    /*
+		     * Esto consigue que sea busqueda exacta queryString =
+		     * "TITLE:" + "\"" + doc.get("TITLE") +"\"" + " AND BODY:" +
+		     * "\"" + doc.get("TITLE") +"\""; query =
+		     * parser.parse(queryString);
+		     */
+
+		    //
+		    bqBuilder = new BooleanQuery.Builder();
+		    StringTokenizer st = new StringTokenizer(doc.get("TITLE"));
+		    while (st.hasMoreTokens()) {
+			String token = st.nextToken();
+			query1 = new TermQuery(new Term("TITLE", token));
+			query2 = new TermQuery(new Term("BODY", token));
+			bqBuilder.add(query1, Occur.SHOULD);
+			bqBuilder.add(query2, Occur.SHOULD);
+		    }
+		    query = bqBuilder.build();
+		    //
+
 		    topDocs = searcher.search(query, 1);
-		    queryDoc = reader.document(topDocs.scoreDocs[0].doc);
-		    
-		    SimPathSgmField = new StringField("SimPathSgm", queryDoc.get("PathSgm"), Field.Store.YES);
+		    System.out.println(topDocs.totalHits);
+
+		    if (topDocs.totalHits == 0) {
+			SimPathSgmField = new StringField("SimPathSgm", "",
+				Field.Store.YES);
+			SimTitle = new TextField("SimTitle", "",
+				Field.Store.YES);
+			SimBody = new TextField("SimBody", "", Field.Store.YES);
+		    } else {
+			queryDoc = reader.document(topDocs.scoreDocs[0].doc);
+			SimPathSgmField = new StringField("SimPathSgm",
+				queryDoc.get("PathSgm"), Field.Store.YES);
+			SimTitle = new TextField("SimTitle",
+				queryDoc.get("TITLE"), Field.Store.YES);
+			System.out.println( queryDoc.get("BODY"));
+			SimBody = new TextField("SimBody", queryDoc.get("BODY"),
+				Field.Store.YES);
+		    }
 		    doc.add(SimPathSgmField);
-		    
-		    SimTitle = new TextField("SimTitle", queryDoc.get("TITLE"), Field.Store.YES);
 		    doc.add(SimTitle);
-		    
-		    SimBody = new TextField("SimBody", queryDoc.get("BODY"),
-			    Field.Store.YES);
 		    doc.add(SimBody);
-		    
+
 		    writer.addDocument(doc);
 		}
-	    } catch (IOException | ParseException e) {
+	    } catch (IOException /* | ParseException */ e) {
 		e.printStackTrace();
 		System.exit(-1);
 	    }
@@ -122,7 +143,8 @@ public class MostSimilarDoc_TitleThreading {
 	    iwc.setOpenMode(OpenMode.CREATE);
 	    IndexWriter writer = new IndexWriter(outdir, iwc);
 
-	    //Each thread will have the first and last document they need to process
+	    // Each thread will have the first and last document they need to
+	    // process
 	    int numDocs = reader.numDocs();
 	    int threadDocs = numDocs / title_threads;
 	    int lastThreadDocs = numDocs - (threadDocs * (title_threads - 1));
@@ -134,16 +156,17 @@ public class MostSimilarDoc_TitleThreading {
 	    }
 	    threadDocRange[title_threads] = threadDocRange[title_threads - 1]
 		    + lastThreadDocs;
-	    
+
 	    final ExecutorService executor = Executors
 		    .newFixedThreadPool(title_threads);
-	    for (int j = 0; j < title_threads -1; j++) {
+	    for (int j = 0; j < title_threads - 1; j++) {
 		final Runnable worker = new WorkerThread(threadDocRange[j],
-			threadDocRange[j + 1]-1, reader, writer);
+			threadDocRange[j + 1] - 1, reader, writer);
 		executor.execute(worker);
 	    }
-	    final Runnable finalworker = new WorkerThread(threadDocRange[title_threads-1],
-			threadDocRange[title_threads], reader, writer);
+	    final Runnable finalworker = new WorkerThread(
+		    threadDocRange[title_threads - 1],
+		    threadDocRange[title_threads], reader, writer);
 	    executor.execute(finalworker);
 
 	    executor.shutdown();
